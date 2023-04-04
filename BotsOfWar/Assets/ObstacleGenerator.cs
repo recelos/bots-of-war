db5e9eb5899d0a386b7603d3f72d55f3404e3254
mic609 <michu-2001@o2.pl>
2023-04-04 21:27:43 +0200

    Changed ObstacleGenerator

--- BotsOfWar/Assets/ObstacleGenerator.cs
+++ BotsOfWar/Assets/ObstacleGenerator.cs
@@ -1,37 +1,45 @@
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class ObstacleGenerator : MonoBehaviour
 {
+    [SerializeField] private GameObject _prefab;
     [SerializeField] private float _density;
+    private Vector3 [] _spawnPoints =
+        {new Vector3(-6.55f, -0.98f, 0),
+        new Vector3(-4.68f, 0.96f, 0),
+        new Vector3(-8.8f, 0.29f, 0),
+        new Vector3(-4.03f, -2.97f, 0),
+        new Vector3(-9.54f, -1.79f, 0),
+        new Vector3(7.48f, -3.27f, 0),
+        new Vector3(8.78f, -0.82f, 0),
+        new Vector3(8.78f, 2.71f, 0),
+        new Vector3(-9.64f, 2.57f, 0),
+        new Vector3(5.83f, 1.03f, 0),
+        new Vector3(5.5f, 3.18f, 0),
+        new Vector3(5.53f, -1.62f, 0),
+        new Vector3(2.64f, 0.39f, 0),
+        new Vector3(0.83f, 3.31f, 0),
+        new Vector3(-1.22f, 0.19f, 0),
+        new Vector3(-2.29f, 3.48f, 0),
+        new Vector3(-6.05f, 3.55f, 0),
+        new Vector3(-7.53f, -3.44f, 0),
+        new Vector3(-0.57f, -3.37f, 0),
+        new Vector3(2.41f, -2.53f, 0)};
 
     private void Start()
     {
-        ToggleObstacles(); // Active random Objects
+        ToggleObstacles();
     }
 
     private void ToggleObstacles()
     {
-        foreach (Transform child in transform)
+        for(int i = 0; i < _spawnPoints.Length; i++)
         {
-            if (child != null)
+            if(Random.value < _density)
             {
-                foreach(Transform grandchild in child)
-                {
-                    if(grandchild != null)
-                    {
-                        if (grandchild.gameObject.GetComponent<SpriteRenderer>() != null)
-                        {
-                            if (Random.value < _density) // _density specifies how probable the objects will spawn
-                            {
-                                grandchild.gameObject.SetActive(true);
-                            }
-                            else
-                            {
-                                grandchild.gameObject.SetActive(false);
-                            }
-                        }
-                    }
-                }
+                GameObject child = Instantiate(_prefab, _spawnPoints[i], Quaternion.identity);
+                child.transform.SetParent(transform);
             }
         }
     }
