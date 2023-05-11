0db01ab4f02c478b4821a6d92271e4c70c814529
Kacper Wróblewski <85016688+kacper651@users.noreply.github.com>
2023-05-11 22:49:22 +0200

    Revert "Kacper/power up generator (#16)"
    This reverts commit 119050182fa216ef4942c25648fadc580c5a2634.

--- BotsOfWar/Assets/Scripts/BasicMovement.cs
+++ BotsOfWar/Assets/Scripts/BasicMovement.cs
@@ -7,17 +7,20 @@ public class BasicMovement : MonoBehaviour
     [SerializeField]private Animator _animator;
     private bool _facesRight;
 
-    public void Update()
+    // Update is called once per frame
+    void Update()
     {
         Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
 
         if (movement.x < 0)
         {
-            GetComponent<SpriteRenderer>().flipX = true;
+            transform.localScale = new Vector3(-1, 1, 1);
+            _facesRight = false;
         }
         else if (movement.x > 0)
         {
-            GetComponent<SpriteRenderer>().flipX = false;
+            transform.localScale = new Vector3(1, 1, 1);
+            _facesRight = true;
         }
 
         _animator.SetFloat("Horizontal", movement.x);
