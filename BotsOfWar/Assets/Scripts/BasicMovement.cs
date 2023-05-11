119050182fa216ef4942c25648fadc580c5a2634
Kacper Wróblewski <85016688+kacper651@users.noreply.github.com>
2023-05-11 22:42:51 +0200

    Kacper/power up generator (#16)
    * Cleaned up BasicMovement.cs
    
    * SetUp for later on dfferent PC
    
    * MainScene changes
    
    * Added fiex spawnpoints in obstacleGenerator
    
    You can now drop down PickUpCoords object to manipulate spawnPoints of obstacles. PickUpCoords is now set as a serialized field in obstacleGenerator script, but change only position of its children game object in order to change the spawn points. To modify the object that is being spawned, modify 'prefeb' field.
    
    * Update MainScene.unity
    
    * Finished logic for power-up pick up as well as animations
    
    For now picking up things doesnt do anything, just destroys the object and plays the animation, but in future it can just make them invisible for some period of time to "respawn" them or something.
    
    * Renamed scripts
    
    Deleted obstacle generator completely, substitued with powerup generator (same thing, more relevant naming)
    
    * Update MainScene.unity
    
    Added powerup generator to the scene
    
    * Deleted old scenes and renamed OLD main scene
    
    * Add MainScene.unity for replacement

--- BotsOfWar/Assets/Scripts/BasicMovement.cs
+++ BotsOfWar/Assets/Scripts/BasicMovement.cs
@@ -7,20 +7,17 @@ public class BasicMovement : MonoBehaviour
     [SerializeField]private Animator _animator;
     private bool _facesRight;
 
-    // Update is called once per frame
-    void Update()
+    public void Update()
     {
         Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
 
         if (movement.x < 0)
         {
-            transform.localScale = new Vector3(-1, 1, 1);
-            _facesRight = false;
+            GetComponent<SpriteRenderer>().flipX = true;
         }
         else if (movement.x > 0)
         {
-            transform.localScale = new Vector3(1, 1, 1);
-            _facesRight = true;
+            GetComponent<SpriteRenderer>().flipX = false;
         }
 
         _animator.SetFloat("Horizontal", movement.x);
