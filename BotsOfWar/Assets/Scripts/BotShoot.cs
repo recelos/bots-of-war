8b2923da5dbb74b99960fce7d4973632f22e4551
Kacper Wróblewski <85016688+kacper651@users.noreply.github.com>
2023-05-25 14:45:58 +0200

    Add FieldOfView.cs (#22)
    * Add FieldOfView.cs
    
    Now bot detects all gameobject within certain radius
    
    * MainScene changes and FIeldOfView optimization
    
    * Cleanup
    
    * Prepare for merge
    
    * Prepare for merge2

--- BotsOfWar/Assets/Scripts/BotShoot.cs
+++ BotsOfWar/Assets/Scripts/BotShoot.cs
@@ -8,27 +8,23 @@ public class BotShoot : MonoBehaviour
     [SerializeField] private float _fireRatePerMinute = 500;
     [SerializeField] private int _bulletSpeed = 400;
     [SerializeField] private int _damage;
-    private BasicMovement _basicMovement;
     private float _timeSinceLastShot;
-    
-    void Start()
-    {
-        _basicMovement =  GetComponent<BasicMovement>();
-    }
 
     void Update()
     {
         //how much time has passed since last shot
         _timeSinceLastShot += Time.deltaTime;
 
-        if(Input.GetMouseButton(0) && CanShoot()){
+        if (Input.GetMouseButton(0) && CanShoot())
+        {
             Shoot();
             _timeSinceLastShot = 0;
         }
-        
+
     }
 
-    private void Shoot(){
+    private void Shoot()
+    {
         //create bullet at shootPoint position with shootPoint rotation
         GameObject bullet = Instantiate(_bullet, this.transform.position, this.transform.rotation);
         GiveSpeedToBullet(bullet);
@@ -36,18 +32,20 @@ public class BotShoot : MonoBehaviour
         bullet.GetComponent<Bullet>().Damage = _damage;
         bullet.GetComponent<Bullet>().Source = this.gameObject;
     }
+
     private void GiveSpeedToBullet(GameObject bullet)
     {
         bullet.GetComponent<Rigidbody2D>().AddForce(((Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition) - (Vector2)this.transform.position).normalized * _bulletSpeed);
     }
     //checks if the bot is able to shoot
-    private bool CanShoot(){
+    private bool CanShoot()
+    {
         return _timeSinceLastShot > 1f / (_fireRatePerMinute / 60f);
     }
 
-    private void OnTriggerEnter2D(Collider2D other) 
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        if(other.CompareTag("PickUp"))
+        if (other.CompareTag("PickUp"))
         {
             _damage += 100;
         }
---
+++ BotsOfWar/Assets/Scripts/FieldOfView.cs
@@ -0,0 +1,81 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class FieldOfView : MonoBehaviour
+{
+    [SerializeField] float _radius = 5f;
+    [SerializeField] float _angle = 360f;
+    [SerializeField] LayerMask _obstacleMask; // Layer mask for obstacles, e.g. walls, bot doesn't see through them
+
+    private void Start()
+    {
+        StartCoroutine(FindTargetsWithDelay(.2f));
+    }
+
+    // Find targets every x seconds, so it doesn't have to be done every frame
+    IEnumerator<WaitForSeconds> FindTargetsWithDelay(float delay)
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(delay);
+            FindVisibleTargets();
+        }
+    }
+
+    // Field of view in 2D space, only used for drawing in editor
+    public Vector2 DirectionFromAngle(float angleInDegrees, bool angleIsGlobal)
+    {
+        if (!angleIsGlobal)
+        {
+            angleInDegrees += transform.eulerAngles.z;
+        }
+        return new Vector2(Mathf.Sin(angleInDegrees * Mathf.Deg2Rad), Mathf.Cos(angleInDegrees * Mathf.Deg2Rad));
+    }
+
+    // Get all targets in radius
+    public List<Transform> FindVisibleTargets()
+    {
+        var visibleTargets = new List<Transform>();
+        Collider2D[] targetsInViewRadius = Physics2D.OverlapCircleAll(transform.position, _radius);
+
+        foreach (Collider2D targetCollider in targetsInViewRadius)
+        {
+            var target = targetCollider.transform;
+
+            // Skip if the target is the same as the object this script is attached to
+            if (target == transform)
+                continue;
+
+            Vector2 dirToTarget = (target.position - transform.position).normalized;
+
+            if (Vector2.Angle(transform.up, dirToTarget) < _angle / 2)
+            {
+                var distanceToTarget = Vector2.Distance(transform.position, target.position);
+
+                // Raycast to check for obstacles between the player and the target
+                RaycastHit2D hit = Physics2D.Raycast(transform.position, dirToTarget, distanceToTarget, _obstacleMask);
+
+                if (hit.collider != null)
+                    continue;
+
+                visibleTargets.Add(target);
+                Debug.Log("Target found: " + target.name);
+            }
+        }
+        return visibleTargets;
+    }
+
+
+    // Draw field of view in editor
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, _radius);
+
+        Vector2 fovLine1 = DirectionFromAngle(-_angle / 2, false);
+        Vector2 fovLine2 = DirectionFromAngle(_angle / 2, false);
+
+        Gizmos.DrawLine(transform.position, (Vector2)transform.position + fovLine1 * _radius);
+        Gizmos.DrawLine(transform.position, (Vector2)transform.position + fovLine2 * _radius);
+    }
+}
